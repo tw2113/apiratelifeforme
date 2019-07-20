80ebf0892baacbdd68ad735c0108efc78ff08afe
Michael Beckwith <tw2113@gmail.com>
2019-07-20 17:26:47 -0500

    enable rel links

--- mu-plugins/utilities.php
+++ mu-plugins/utilities.php
@@ -12,7 +12,6 @@ add_action('init', function(){
 	remove_action('wp_head', 'rsd_link');
 	remove_action('wp_head', 'wlwmanifest_link');
 	remove_action('wp_head', 'wp_generator');
-	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 });
 
 //Remove <p> tags from images
