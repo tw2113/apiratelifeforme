76bc74b2ea67e63dcd74a5a6a786f83e9e8c78fb
Michael Beckwith <tw2113@gmail.com>
2019-08-27 01:05:57 -0500

    missed semicolon

--- themes/autonomie-child/functions.php
+++ themes/autonomie-child/functions.php
@@ -56,6 +56,6 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\scripts_styles' );
 
 function credits() {
 	echo wpautop( 'We are wolves of the sea.' );
-	echo '<a href="https://xn--sr8hvo.ws/3%EF%B8%8F%E2%83%A3%F0%9F%93%B2%F0%9F%8D%A3/previous">←</a>An IndieWeb Webring 🕸💍<a href="https://xn--sr8hvo.ws/3%EF%B8%8F%E2%83%A3%F0%9F%93%B2%F0%9F%8D%A3/next">→</a>'
+	echo '<a href="https://xn--sr8hvo.ws/3%EF%B8%8F%E2%83%A3%F0%9F%93%B2%F0%9F%8D%A3/previous">←</a>An IndieWeb Webring 🕸💍<a href="https://xn--sr8hvo.ws/3%EF%B8%8F%E2%83%A3%F0%9F%93%B2%F0%9F%8D%A3/next">→</a>';
 }
 add_action( 'autonomie_credits', __NAMESPACE__ . '\credits' );
