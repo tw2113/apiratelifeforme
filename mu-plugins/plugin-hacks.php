9111bac84a9be7359c362e461ff8c617a8c45270
Michael Beckwith <tw2113@gmail.com>
2019-08-30 18:23:09 -0500

    stupid arrays

--- mu-plugins/plugin-hacks.php
+++ mu-plugins/plugin-hacks.php
@@ -2,7 +2,7 @@
 namespace tw2113;
 
 function override_zerospam_for_webmentions( $commentdata ) {
-	if ( function_exists( 'zerospam_get_key' ) && '/wp-json/webmention/1.0/endpoint' === $_SERVER['REQUEST_URI'] && 'webmention' === $commentdata->comment_type ) {
+	if ( function_exists( 'zerospam_get_key' ) && '/wp-json/webmention/1.0/endpoint' === $_SERVER['REQUEST_URI'] && 'webmention' === $commentdata['comment_type'] ) {
 		$_POST['zerospam_key'] = zerospam_get_key();
 	}
 	return $commentdata;
