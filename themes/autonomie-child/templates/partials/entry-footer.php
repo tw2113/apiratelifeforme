41ebc86cd3a0e52c0033410bc40053e6b37e751b
Michael Beckwith <tw2113@gmail.com>
2019-08-29 22:54:59 -0500

    remove category output in favor of tags

--- themes/autonomie-child/templates/partials/entry-footer.php
+++ themes/autonomie-child/templates/partials/entry-footer.php
@@ -14,16 +14,6 @@
         <?php
         endif;
 
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list();
-		if ( $categories_list ) :
-		?>
-		<div class="cat-links">
-			<?php echo __( 'Categories', 'autonomie' ); ?>
-			<?php printf( __( '%1$s', 'autonomie' ), $categories_list ); ?>
-		</div>
-		<?php endif; // End if categories ?>
-
 		<?php
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '<ul><li>', '</li><li>', '</li></ul>' );
