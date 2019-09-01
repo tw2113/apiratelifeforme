470e867f78688e126f0700ba7d3a1a7fbd7c1374
Michael Beckwith <tw2113@gmail.com>
2019-08-31 22:01:44 -0500

    declare our feed for last.fm which has no actual data yet

--- plugins/yarrrss/src/LastFM/LastFM.php
+++ plugins/yarrrss/src/LastFM/LastFM.php
@@ -8,13 +8,27 @@ class LastFM extends Base implements BaseInterface {
 
     public function __construct( \WP_Query $wp_query ) {
         $this->wp_query = $wp_query;
+        $this->classname = $this->get_class_name_base();
+
+        parent::__construct();
     }
 
     public function hooks() {
+        add_action( 'init', [ $this, 'declareFeed' ] );
+    }
 
+    public function declareFeed() {
+        $feed = strtolower( $this->classname );
+        add_feed( $feed, [ $this, 'loadTemplate' ] );
     }
 
     public function loadTemplate() {
+        require_once $this->dir . "templates/{$this->classname}.php";
+    }
 
+    public function get_class_name_base() {
+        $name = explode('\\', __CLASS__);
+
+        return array_pop( $name );
     }
 }
