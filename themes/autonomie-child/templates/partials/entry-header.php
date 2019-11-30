63d7dc735f73752e4b39ee7d9c7d10f4ef2e39f0
Michael Beckwith <tw2113@gmail.com>
2019-11-30 14:43:17 -0600

    i want titles on all post formats

---
+++ themes/autonomie-child/templates/partials/entry-header.php
@@ -0,0 +1,28 @@
+	<header class="entry-header">
+		<div class="entry-header-wrapper">
+			<?php if ( ! is_page() ) : ?>
+			<div class="entry-meta post-format">
+				<?php echo apply_filters( 'autonomie_post_format', sprintf(
+					'<a class="entry-format entry-format-%s" href="%s">%s</a>',
+					autonomie_get_post_format(),
+					esc_url( autonomie_get_post_format_link( autonomie_get_post_format() ) ),
+					autonomie_get_post_format_string()
+				) ); ?>
+			</div>
+			<?php endif; ?>
+
+			<h1 class="entry-title p-name" itemprop="name headline">
+				<a href="<?php the_permalink(); ?>" class="u-url url" title="<?php printf( esc_attr__( 'Permalink to %s', 'autonomie' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url">
+					<?php the_title(); ?>
+				</a>
+			</h1>
+
+			<?php // if ( ! is_singular() ) : ?>
+			<div class="entry-meta">
+				<?php autonomie_posted_on(); ?>
+			</div>
+			<?php // endif; ?>
+		</div>
+	</header><!-- .entry-header -->
+
+	<?php do_action( 'autonomie_before_entry_content' ); ?>
