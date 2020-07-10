49a9b98549e7e6f22ca49939a67ea13b8588b9bb
Michael Beckwith <tw2113@gmail.com>
2020-07-10 00:43:52 -0500

    start out our goodreads mimicry

---
+++ plugins/pirate-book-chest/pirate-book-chest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Pirate Book Chest
+ * Plugin URI: https://apiratelifefor.me
+ * Description: GoodReads, my own version
+ * Author: tw2113
+ * Author URI: https://michaelbox.net
+ * Version: 1.0.0
+ */
+
+use tw2113\pbc;
+
