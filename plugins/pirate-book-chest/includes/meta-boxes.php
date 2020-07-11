7af6df03ebbbf5e3783876e1507e1fe4a4b64f9c
Michael Beckwith <tw2113@gmail.com>
2020-07-11 14:56:01 -0500

    so many new content types and metaboxes for it all

--- plugins/pirate-book-chest/includes/meta-boxes.php
+++ plugins/pirate-book-chest/includes/meta-boxes.php
@@ -4,27 +4,115 @@ namespace tw2113\pbc;
 
 function meta_boxes() {
 	$prefix = 'pbc';
-	$cmbpbc = new_cmb2_box( [
-		'id'           => $prefix . '_book_metabox',
-		'title'        => esc_html__( 'Book Details', 'cmb2' ),
+	$cmbpbc_reading = new_cmb2_box( [
+		'id'           => $prefix . '_book_reading_metabox',
+		'title'        => esc_html__( 'Book Reading Details', 'pirate-book-chest' ),
 		'object_types' => [ 'books' ],
 		'context'      => 'normal',
 		'priority'     => 'high',
 	] );
 
-	$cmbpbc->add_field( [
-		'name'       => esc_html__( 'Current page', 'cmb2' ),
-		'desc'       => esc_html__( 'What page are you on?', 'cmb2' ),
+	$cmbpbc_reading->add_field( [
+		'name'       => esc_html__( 'Current page', 'pirate-book-chest' ),
+		'desc'       => esc_html__( 'What page are you on?', 'pirate-book-chest' ),
 		'id'         => $prefix . '_current_page',
 		'type'       => 'text_small',
 	] );
 
-	$cmbpbc->add_field( [
-		'name'       => esc_html__( 'Total page', 'cmb2' ),
-		'desc'       => esc_html__( 'How many pages does the book have?', 'cmb2' ),
+	$cmbpbc_reading->add_field( [
+		'name'       => esc_html__( 'Total pages', 'pirate-book-chest' ),
+		'desc'       => esc_html__( 'How many pages does the book have?', 'pirate-book-chest' ),
 		'id'         => $prefix . '_total_pages',
 		'type'       => 'text_small',
 	] );
 
+	$cmbpbc_reading->add_field( [
+		'name'             => 'Reading status',
+		'desc'             => 'What is the status?',
+		'id'               => $prefix . '_reading_status',
+		'taxonomy'         => 'book_status',
+		'type'             => 'taxonomy_radio',
+		'remove_default'   => 'true',
+		'show_option_none' => false,
+	] );
+
+	$cmbpbc_reading->add_field( [
+		'name'           => 'Current book chests',
+		'desc'           => 'Which chests do this book belong to?',
+		'id'             => $prefix . '_book_chest',
+		'taxonomy'       => 'book_chest',
+		'type'           => 'taxonomy_multicheck',
+		'remove_default' => 'true',
+	] );
+
+	$cmbpbc_reviews = new_cmb2_box( [
+		'id'           => $prefix . '_book_reviews_metabox',
+		'title'        => esc_html__( 'Book Review Details', 'pirate-book-chest' ),
+		'object_types' => [ 'books' ],
+		'context'      => 'normal',
+		'priority'     => 'high',
+	] );
+
+	$cmbpbc_reviews->add_field( [
+		'name'           => 'Book Review',
+		'id'             => $prefix . '_book_review',
+		'type'           => 'book_review',
+	] );
+
+	$cmbpbc_reviews->add_field( [
+		'name'    => 'Rating',
+		'id'      => $prefix . '_rating',
+		'type'    => 'select',
+		'options' => [
+			'rating1' => '1 star',
+			'rating2' => '2 stars',
+			'rating3' => '3 stars',
+			'rating4' => '4 stars',
+			'rating5' => '5 stars',
+		],
+	] );
+
+
+
+	$cmbpbc_details = new_cmb2_box( [
+		'id'           => $prefix . '_book_details_metabox',
+		'title'        => esc_html__( 'Book Information', 'pirate-book-chest' ),
+		'object_types' => [ 'books' ],
+		'context'      => 'side',
+		'priority'     => 'default',
+	] );
+
+	$cmbpbc_details->add_field( [
+		'name'           => 'Genre',
+		'id'             => $prefix . '_book_genre',
+		'taxonomy'       => 'genre',
+		'type'           => 'taxonomy_multicheck',
+		'remove_default' => 'true',
+	] );
 }
 add_action( 'cmb2_admin_init', __NAMESPACE__ . '\meta_boxes' );
+
+
+function cmb2_render_book_review( $field, $escaped_value, $object_id,
+								 $object_type, $field_type_object ) {
+
+	$comments = get_comments(
+		[
+			'post_id' => $object_id
+		]
+	);
+
+	if ( empty( $comments ) ) {
+		echo 'No reviews yet';
+		return;
+	}
+
+	foreach ( $comments as $comment ) {
+		printf( '<h3>Rating: %s star</h3>', get_comment_meta( $comment->comment_ID, 'pbc_rating', true ) );
+
+		echo wpautop( $comment->comment_content );
+	}
+
+
+}
+add_action( 'cmb2_render_book_review', __NAMESPACE__ . '\cmb2_render_book_review', 10, 5 );
--- plugins/pirate-book-chest/includes/types-taxes.php
+++ plugins/pirate-book-chest/includes/types-taxes.php
@@ -2,19 +2,16 @@
 
 namespace tw2113\pbc;
 
-function cptui_register_my_cpts_books() {
-
-	/**
-	 * Post Type: Books.
-	 */
+function register_my_cpts() {
 
 	$labels = [
-		"name" => __( "Books", "custom-post-type-ui" ),
-		"singular_name" => __( "Book", "custom-post-type-ui" ),
+		"name" => __( "Books", "pirate-book-chest" ),
+		"singular_name" => __( "Book", "pirate-book-chest" ),
+		"featured_image" => __( 'Book Image', 'pirate-book-chest' ),
 	];
 
 	$args = [
-		"label" => __( "Books", "custom-post-type-ui" ),
+		"label" => __("Books", "pirate-book-chest"),
 		"labels" => $labels,
 		"description" => "",
 		"public" => true,
@@ -31,12 +28,85 @@ function cptui_register_my_cpts_books() {
 		"capability_type" => "post",
 		"map_meta_cap" => true,
 		"hierarchical" => false,
-		"rewrite" => [ "slug" => "books", "with_front" => true ],
+		"rewrite" => ["slug" => "books", "with_front" => true],
 		"query_var" => true,
 		"menu_icon" => "dashicons-book",
-		"supports" => [ "title", "editor", "thumbnail" ],
+		"supports" => ["title", "editor", "thumbnail", "comments"],
 	];
 
 	register_post_type( "books", $args );
 }
-add_action( 'init', __NAMESPACE__ . '\cptui_register_my_cpts_books' );
+add_action( 'init', __NAMESPACE__ . '\register_my_cpts' );
+
+function register_my_books_taxes() {
+
+	$labels = [
+		"name"          => __( "Status", "pirate-book-chest" ),
+		"singular_name" => __( "Status", "pirate-book-chest" ),
+	];
+
+	$args = [
+		"label" => __( "Status", "pirate-book-chest" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => false,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'book_status', 'with_front' => true, ],
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"rest_base" => "book_status",
+		"show_in_quick_edit" => false,
+	];
+	register_taxonomy( "book_status", [ "books" ], $args );
+
+	$labels = [
+		"name"          => __( "Book Chests", "pirate-book-chest" ),
+		"singular_name" => __( "Book Chest", "pirate-book-chest" ),
+	];
+
+	$args = [
+		"label" => __( "Book Chest", "pirate-book-chest" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'book_chest', 'with_front' => true, ],
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"rest_base" => "book_chest",
+		"show_in_quick_edit" => false,
+	];
+	register_taxonomy( "book_chest", [ "books" ], $args );
+
+	$labels = [
+		"name"          => __( "Genre", "pirate-book-chest" ),
+		"singular_name" => __( "Genre", "pirate-book-chest" ),
+	];
+
+	$args = [
+		"label" => __( "Genre", "pirate-book-chest" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'genre', 'with_front' => true, ],
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"rest_base" => "genre",
+		"show_in_quick_edit" => false,
+	];
+	register_taxonomy( "genre", [ "books" ], $args );
+}
+add_action( 'init', __NAMESPACE__ . '\register_my_books_taxes' );
