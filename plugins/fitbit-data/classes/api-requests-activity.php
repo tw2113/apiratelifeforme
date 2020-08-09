ec7594d64df4df826db87909302e31b768a791f3
Michael Beckwith <tw2113@gmail.com>
2020-08-09 14:31:11 -0500

    progress

--- plugins/fitbit-data/classes/api-requests-activity.php
+++ plugins/fitbit-data/classes/api-requests-activity.php
@@ -5,6 +5,13 @@ use tw2113\fitbit\API\API;
 
 class Activity extends API {
 
+	private $endpoint = 'https://api.fitbit.com/1/user/-/activities/date/[date].json';
+
+	private $lifetime_activity_endpoint = 'https://api.fitbit.com/1/user/[user-id]/activities.json';
+
+	# /1/user/[user-id]/[resource-path]/date/[date]/[period].json
+	# /1/user/[user-id]/[resource-path]/date/[base-date]/[end-date].json
+
 	/**
 	 * API constructor.
 	 *
@@ -20,6 +27,8 @@ class Activity extends API {
 			$date = $dt->format('Y-m-d');
 		}
 
+		$endpoint = str_replace( '[date]', $date, $this->endpoint );
+
 		$request_args = [
 			'timeout' => 120,
 			'headers' => [
@@ -28,7 +37,35 @@ class Activity extends API {
 		];
 
 		$result = wp_remote_get(
-			"https://api.fitbit.com/1/user/-/activities/date/{$date}.json",
+			$endpoint,
+			$request_args
+		);
+
+		if ( is_wp_error( $result ) ) {
+
+			$error = sprintf(
+				__( 'WordPress error: %s', 'fitbit-api' ),
+				$result->get_error_message()
+			);
+			return $error;
+		}
+
+		$response = json_decode( wp_remote_retrieve_body( $result ) );
+		if ( 200 === wp_remote_retrieve_response_code( $result ) ) {
+			return $response;
+		}
+	}
+
+	public function get_lifetime_activity() {
+		$request_args = [
+			'timeout' => 120,
+			'headers' => [
+				'Authorization' => 'Bearer ' . $this->access_token
+			]
+		];
+
+		$result = wp_remote_get(
+			$this->lifetime_activity_endpoint,
 			$request_args
 		);
 
