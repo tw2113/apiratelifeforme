0902eaafdf7d07da32422ddeeef96933a4719692
Michael Beckwith <tw2113@gmail.com>
2020-08-12 02:12:16 -0500

    add a search for easier finding for probably myself and maybe others

---
+++ themes/autonomie-child/book-search.php
@@ -0,0 +1,9 @@
+<form itemprop="potentialAction" itemscope="" itemtype="http://schema.org/SearchAction" role="search" method="get" class="search-form" action="https://apiratelifefor.me/">
+	<label>
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" itemprop="query-input" class="search-field" placeholder="Search my book chests..." value="" name="s">
+	</label>
+	<input type="hidden" name="post_type" value="books" />
+	<input type="submit" class="search-submit" value="Search">
+	<meta itemprop="target" content="https://apiratelifefor.me/?s={search} ">
+</form>
