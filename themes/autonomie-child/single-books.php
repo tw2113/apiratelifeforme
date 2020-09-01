45ee589ef0c57d797298eb7e83c33e8cbb0f67db
Michael Beckwith <tw2113@gmail.com>
2020-08-31 23:14:11 -0500

    borrow from micro.blog for lookups without direct links

--- themes/autonomie-child/single-books.php
+++ themes/autonomie-child/single-books.php
@@ -94,7 +94,7 @@ get_header(); ?>
                     printf( '<p><strong>Author:</strong> %s</p>', $authors );
                 }
                 if ( $isbn ) {
-                    printf( '<p><strong>ISBN13:</strong> %s</p>', $isbn );
+                    printf( '<p><strong>ISBN13:</strong> <a href="https://www.amazon.com/s?field-keywords=%s">%s</a></p>', $isbn, $isbn );
                 }
 
 				echo get_the_term_list( get_the_ID(), 'genre', '<p><strong>Genres:</strong> ', ', ', '</p>' );
