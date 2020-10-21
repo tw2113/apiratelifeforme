26553382956d3c82f051f449f098ee0539d6c4d4
Michael Beckwith <tw2113@gmail.com>
2020-10-21 01:34:30 -0500

    amend our layout based on list type

--- themes/autonomie-child/archive-music_video.php
+++ themes/autonomie-child/archive-music_video.php
@@ -16,16 +16,40 @@ get_header(); ?>
 <main id="primary">
     <article>
         <div class="entry-content e-content" itemprop="description articleBody">
-            <?php while ( have_posts() ) : the_post(); ?>
-            <article <?php autonomie_post_id(); ?> <?php post_class(); ?><?php autonomie_semantics( 'post' ); ?>>
-                <h1><?php the_title(); ?></h1>
+            <?php
+            if ( empty( $_GET ) ) {
+                while ( have_posts() ) : the_post(); ?>
+                <article <?php autonomie_post_id(); ?> <?php post_class(); ?><?php autonomie_semantics( 'post' ); ?>>
+                    <h1><?php the_title(); ?></h1>
 
-                <?php the_content(); ?>
+                    <?php the_content(); ?>
 
-                <a href="<?php echo get_post_type_archive_link( 'music_video' ); ?>">Random video</a>
+                    <p><a href="<?php echo esc_url( get_post_type_archive_link( 'music_video' ) ); ?>">Random video</a></p>
 
-            <?php endwhile; // end of the loop. ?>
-            </article>
+                    <p><a href="<?php echo esc_url( add_query_arg( ['list' => 'all'], get_post_type_archive_link( 'music_video' ) ) ); ?>">List of all available</a></p>
+                </article>
+                <?php endwhile;
+            }
+            if ( ! empty( $_GET ) && 'all' === $_GET['list'] ) {
+                ?>
+                <ul>
+                <?php
+                while ( have_posts() ) : the_post(); ?>
+                <li <?php autonomie_post_id(); ?> <?php post_class(); ?><?php autonomie_semantics( 'post' ); ?>>
+                    <?php
+					printf(
+                        '<a href="%s">%s</a>',
+                        get_the_permalink(),
+                        get_the_title()
+                    );
+                    ?>
+                </li>
+                <?php
+                endwhile; ?>
+                </ul>
+            <?php
+			}
+            ?>
         </div>
     </article>
 </main><!-- #content -->
