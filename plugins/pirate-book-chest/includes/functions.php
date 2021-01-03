37f868e1ffc62688dab86908db7ff68150fc062c
Michael Beckwith <tw2113@gmail.com>
2021-01-03 15:55:32 -0600

    shortcode to list out our challenge years

--- plugins/pirate-book-chest/includes/functions.php
+++ plugins/pirate-book-chest/includes/functions.php
@@ -51,3 +51,25 @@ function the_shortest_book() {
 
 	return 'n/a';
 }
+
+function get_reading_challenge_years() {
+	$args = [
+		'post_type' => 'book-challenges',
+		'posts_per_page' => -1,
+	];
+
+	$challenges = new \WP_Query( $args );
+	$content = '';
+	if ( $challenges->have_posts() ) : while ( $challenges->have_posts() ) : $challenges->the_post();
+	ob_start();
+	?>
+	<h2><a href="<?php echo esc_attr( get_the_permalink() ); ?>"><?php the_title(); ?></a></h2>
+	<?php
+	$content .= ob_get_clean();
+	endwhile;
+	wp_reset_postdata();
+	endif;
+
+	return $content;
+}
+add_shortcode( 'read-challenges', __NAMESPACE__ . '\get_reading_challenge_years' );
