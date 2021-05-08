2e3259ac8cd16e2600736de8af2b425424b83d4a
Michael Beckwith <tw2113@gmail.com>
2021-05-08 02:20:17 -0500

    video archive seo touchups

--- themes/autonomie-child/functions.php
+++ themes/autonomie-child/functions.php
@@ -208,3 +208,21 @@ function music_video_archive_title( $title ) {
 	return $title;
 }
 add_filter( 'get_the_archive_title', __NAMESPACE__ . '\music_video_archive_title' );
+
+function music_vide_meta_title( $description ) {
+	if ( ! is_post_type_archive( 'music_video' ) ) {
+		return $description;
+	}
+
+	return 'The Pirate Asylum Radio Archives';
+}
+add_filter( 'the_seo_framework_title_from_generation', __NAMESPACE__ . '\music_vide_meta_title' );
+
+function music_video_meta_description( $description ) {
+	if ( ! is_post_type_archive( 'music_video' ) ) {
+		return $description;
+	}
+
+	return 'Michael Beckwith\'s "Radio" archive, a curated playlist of videos.';
+}
+add_filter( 'the_seo_framework_pta_description', __NAMESPACE__ . '\music_video_meta_description' );
