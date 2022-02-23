becec3ed34899d4069adbed28395b7934d4be198
Michael Beckwith <tw2113@gmail.com>
2022-02-23 00:32:32 -0600

    more algolia touchups

--- themes/autonomie-child/functions.php
+++ themes/autonomie-child/functions.php
@@ -226,3 +226,14 @@ function music_video_meta_description( $description ) {
 	return 'Michael Beckwith\'s "Radio" archive, a curated playlist of videos.';
 }
 add_filter( 'the_seo_framework_pta_description', __NAMESPACE__ . '\music_video_meta_description' );
+
+function algolia_exclude_post_types( $post_types ) {
+
+	// Ignore these post types.
+	unset( $post_types['tumblr'] );
+	unset( $post_types['coffee_checkins'] );
+	unset( $post_types['presentation'] );
+
+	return $post_types;
+}
+add_filter( 'algolia_searchable_post_types', __NAMESPACE__ . '\algolia_exclude_post_types' );
