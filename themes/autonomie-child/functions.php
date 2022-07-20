1ec98128fc447d968003715e93682afbf9a533a7
Michael Beckwith <tw2113@gmail.com>
2022-07-20 01:32:26 -0500

    add author meta to searchable attributes

--- themes/autonomie-child/functions.php
+++ themes/autonomie-child/functions.php
@@ -244,3 +244,22 @@ function algolia_exclude_post_types( $post_types ) {
 	return $post_types;
 }
 add_filter( 'algolia_searchable_post_types', __NAMESPACE__ . '\algolia_exclude_post_types' );
+
+function algolia_custom_fields( array $attributes, \WP_Post $post ) {
+
+	$fields = [
+		'pbc_book_authors',
+	];
+
+	foreach ( $fields as $field ) {
+		$data = get_post_meta( $post->ID, $field );
+
+		if ( ! empty( $data )  ) {
+			$attributes[ $field ] = $data;
+		}
+	}
+
+	return $attributes;
+}
+add_filter( 'algolia_post_shared_attributes', __NAMESPACE__ . '\algolia_custom_fields', 10, 2 );
+add_filter( 'algolia_searchable_post_shared_attributes', __NAMESPACE__ . '\algolia_custom_fields', 10, 2 );
