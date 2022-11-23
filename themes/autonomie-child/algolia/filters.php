941876ed27439e1e500b6408b9d5397c33cd661b
Michael Beckwith <tw2113@gmail.com>
2022-11-22 21:54:13 -0600

    move algolia functionality to dedicated files

---
+++ themes/autonomie-child/algolia/filters.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace tw2113;
+
+function algolia_exclude_post_types( $post_types ) {
+
+	// Ignore these post types.
+	unset( $post_types['tumblr'] );
+	unset( $post_types['coffee_checkins'] );
+	unset( $post_types['presentation'] );
+
+	return $post_types;
+}
+add_filter( 'algolia_searchable_post_types', __NAMESPACE__ . '\algolia_exclude_post_types' );
+
+function algolia_custom_fields( array $attributes, \WP_Post $post ) {
+
+	$fields = [
+		'pbc_book_authors',
+	];
+
+	foreach ( $fields as $field ) {
+		$data = get_post_meta( $post->ID, $field );
+
+		if ( ! empty( $data )  ) {
+			$attributes[ $field ] = $data;
+		}
+	}
+
+	unset( $attributes['post_author'] );
+
+	return $attributes;
+}
+add_filter( 'algolia_post_shared_attributes', __NAMESPACE__ . '\algolia_custom_fields', 10, 2 );
+add_filter( 'algolia_searchable_post_shared_attributes', __NAMESPACE__ . '\algolia_custom_fields', 10, 2 );
+
+function posts_index_settings( $settings ) {
+
+	$settings['searchableAttributes'] = [
+		'unordered(pbc_book_authors)',
+		'unordered(post_title)',
+		'unordered(taxonomies)',
+		'unordered(content)',
+	];
+
+	return $settings;
+}
+add_filter( 'algolia_posts_index_settings', __NAMESPACE__ . '\posts_index_settings' );
+add_filter( 'algolia_posts_books_index_settings', __NAMESPACE__ . '\posts_index_settings' );
--- themes/autonomie-child/functions.php
+++ themes/autonomie-child/functions.php
@@ -2,6 +2,11 @@
 
 namespace tw2113;
 
+function algolia_includes() {
+    require_once get_stylesheet_directory() . '/algolia/filters.php';
+}
+add_action( 'after_setup_theme', __NAMESPACE__ . '\algolia_includes' );
+
 function scripts_styles() {
 
 	$debug = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG == true;
@@ -233,47 +238,3 @@ function music_video_meta_description( $description ) {
 	return 'Michael Beckwith\'s "Radio" archive, a curated playlist of videos.';
 }
 add_filter( 'the_seo_framework_pta_description', __NAMESPACE__ . '\music_video_meta_description' );
-
-function algolia_exclude_post_types( $post_types ) {
-
-	// Ignore these post types.
-	unset( $post_types['tumblr'] );
-	unset( $post_types['coffee_checkins'] );
-	unset( $post_types['presentation'] );
-
-	return $post_types;
-}
-add_filter( 'algolia_searchable_post_types', __NAMESPACE__ . '\algolia_exclude_post_types' );
-
-function algolia_custom_fields( array $attributes, \WP_Post $post ) {
-
-	$fields = [
-		'pbc_book_authors',
-	];
-
-	foreach ( $fields as $field ) {
-		$data = get_post_meta( $post->ID, $field );
-
-		if ( ! empty( $data )  ) {
-			$attributes[ $field ] = $data;
-		}
-	}
-
-	return $attributes;
-}
-add_filter( 'algolia_post_shared_attributes', __NAMESPACE__ . '\algolia_custom_fields', 10, 2 );
-add_filter( 'algolia_searchable_post_shared_attributes', __NAMESPACE__ . '\algolia_custom_fields', 10, 2 );
-
-function posts_index_settings( $settings ) {
-
-	$settings['searchableAttributes'] = [
-        'unordered(pbc_book_authors)',
-        'unordered(post_title)',
-		'unordered(taxonomies)',
-		'unordered(content)',
-    ];
-
-	return $settings;
-}
-add_filter( 'algolia_posts_index_settings', __NAMESPACE__ . '\posts_index_settings' );
-add_filter( 'algolia_posts_books_index_settings', __NAMESPACE__ . '\posts_index_settings' );
